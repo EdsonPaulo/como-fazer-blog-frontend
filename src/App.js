b4dfe921b6632ee7e29d776febe94e9656504796
EdsonPaulo <edsonpaulo24@gmail.com>
2021-03-17 05:14:03 +0100

    Deploy and Customizations

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>B R E V E M E N T E</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/comofazer2021/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visite-nos pelo facebook da Como Fazer?
         </a>
       </header>
     </div>
